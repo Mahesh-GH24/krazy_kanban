2b309242a54b0f3f29241e50eb1149870362f381
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2025-01-12 13:21:45 -0600

    Updated client side changes to authAPI.tsx and auth.ts files

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,31 +1,53 @@
-import { JwtPayload, jwtDecode } from 'jwt-decode';
+//import { JwtPayload, jwtDecode } from 'jwt-decode';
 
 class AuthService {
-  getProfile() {
-    // TODO: return the decoded token
-  }
+
+  // Not used / called anywhere
+  // getProfile() { 
+  //   // TODO: return the decoded token ***********************************************
+  // }
 
   loggedIn() {
+
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token;
+
   }
-  
-  isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
-  }
+  // Not used or called anywhere
+  // isTokenExpired(token: string) { 
+  //   // TODO: return a value that indicates if the token is expired ***********************************************
+
+  // }
 
   getToken(): string {
+
     // TODO: return the token
+    const loggedInUser = localStorage.getItem('id_token') || '';
+    return loggedInUser;
+
   }
 
   login(idToken: string) {
+    
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
+
     // TODO: redirect to the home page
+    window.location.assign('/');
+
   }
 
   logout() {
+    
     // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
+    
     // TODO: redirect to the login page
+    window.location.assign('/');
+
   }
 }
 
+// Export an instance of the AuthService class
 export default new AuthService();
